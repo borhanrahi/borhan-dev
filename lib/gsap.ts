7a1b6019bc9611ca406e3deb1e8113fe2e99c7d0
Borhan <borhanrahi111@gmail.com>
2024-12-11 15:59:33 +0600

    a

--- lib/gsap.ts
+++ lib/gsap.ts
@@ -3,18 +3,11 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TextPlugin } from "gsap/TextPlugin";
 import { ScrambleTextPlugin } from "gsap-trial/ScrambleTextPlugin";
-import { SplitText } from "gsap-trial/SplitText";
+// import { SplitText } from "gsap-trial/SplitText";
 import { useGSAP } from "@gsap/react";
 
 if (typeof window !== "undefined") {
   gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrambleTextPlugin, SplitText);
 }
 
-export {
-  gsap,
-  ScrollTrigger,
-  TextPlugin,
-  ScrambleTextPlugin,
-  SplitText,
-  useGSAP,
-};
+export { gsap, ScrollTrigger, TextPlugin, ScrambleTextPlugin, useGSAP };
