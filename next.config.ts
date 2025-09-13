23290538d54ebea3ebc1fcef8d28ca7e95c31142
borhanrahi <borhanrahi111@gmail.com>
2025-09-13 22:28:45 +0600

    asd

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  trailingSlash: false,
+  generateEtags: false,
   images: {
     remotePatterns: [],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
